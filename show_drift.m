94f4a2eda7cfc67f918ee35a1b9f2efc41f13f03
Alexander Allen <admin@altechcode.com>
2020-03-27 03:00:27 -0400

    Fixed simulation bugs and implemented emptying time simulation analysis

--- show_drift.m
+++ show_drift.m
@@ -11,62 +11,53 @@ oldsrc = src;
 
 % Constants
 c = 3e8;
-factor = 1.820618101874320e-30;
-m = 9.109e-31;
+mass = 9.10938363e-31;
 v = 2.59627974e8;
-gamma = 1/sqrt(1 - (v^2)/(c^2));
-q = 1.602e-19;
+gamma = 2;
+q = 1.60217663e-19;
 E = 200000;
 B = 1;
+theta = -90;
+R = [cosd(theta) -sind(theta); sind(theta) cosd(theta)];
 
-% Track cumulative error
-errc = [];
-err = [];
+vtan = v.*vecnorm([track.initial_momentum_x track.initial_momentum_y], 2, 2)./... 
+    vecnorm([track.initial_momentum_x track.initial_momentum_y track.initial_momentum_z], 2, 2);
 
-% Sample entire particle store
-for i=1:1000
-    
-    % Get particle path data and plot x/y plane projection (for simple viz)
-    step_data = step(map.FIRST_STEP_INDEX(i) + 1 : map.LAST_STEP_INDEX(i), : );
-    %plot(step_data.position_x, step_data.position_y); hold on;
-    
-    cps = [];
-    
-    % Get initial center from particle generator
-    track_data = track(map.TRACK_INDEX(i) + 1, : );
-    pos = [track_data.initial_position_x track_data.initial_position_y];
-    mom = [track_data.initial_momentum_x track_data.initial_momentum_y];
-    
-    v0 = norm(mom) / factor;
-    lamar = gamma*m*v0/(q*B);
-    
-    nmom = mom./norm(mom);
-    theta = -90;
-    R = [cosd(theta) -sind(theta); sind(theta) cosd(theta)];
-    ic = (nmom*R)*lamar + pos;
-    
-    % Get centers from particle path data
-    for j=1:height(step_data)
-        
-        % Calculate theoretical center point given lamar radius
-        px = [step_data(j,:).position_x step_data(j,:).position_y];
-        v_i = [step_data.momentum_x(j) step_data.momentum_y(j)];
-        v_i = v_i./norm(v_i);
-        theta = -90;
-        R = [cosd(theta) -sind(theta); sind(theta) cosd(theta)];
-        cp = v_i*R;
-        cp = cp * lamar;
-        cp = cp + px;
-        cpn = cp + [(E/B)*step_data(j,:).time 0]; % Attempt a correction
-        cps = [cpn; cp];
-        err = [err; norm(cp - ic)];
-        errc = [errc; norm(cpn - ic)];
-    end
-    
-    % Option to plot predicted centers
-    %plot(cps(:,1),cps(:,2)); 
-end
+larmor = gamma*mass*vtan/(q*B);
+
+ic = ([track.initial_momentum_x track.initial_momentum_y]./vecnorm([track.initial_momentum_x track.initial_momentum_y], 2, 2))*R...
+    .*larmor...
+    + [track.initial_position_x track.initial_position_y];
+
+
+m = arrayfun(@(x,y,z)  repmat(x + 1, z - y + 1, 1), map.TRACK_INDEX, map.FIRST_STEP_INDEX, map.LAST_STEP_INDEX, 'UniformOutput', false);
+m = vertcat(m{:});
+
+l = arrayfun(@(x) larmor(m(x)), (1:1:size(step.momentum_x, 1)).');
+c = cell2mat(arrayfun(@(x) ic(m(x),:), (1:1:size(step.momentum_x, 1)).', 'UniformOutput', false));
+
+cp = (([step.momentum_x step.momentum_y]./vecnorm([step.momentum_x step.momentum_y], 2, 2))*R).*l...
+    + [step.position_x step.position_y];
+
+err = vecnorm(cp - c, 2, 2);
+errc = vecnorm(cp - c + [(E/B)*step.time, zeros(size(step, 1), 1)], 2, 2); % Drift Correction
+
+
 
 % Plot histogram of lamar radius errors
 subplot(1, 2, 1); hist(err, 50); title("Raw Error");
+xlabel("Distance Between Predicted and Simulated Center (m)");
+ylabel("Count (N = 2.3 mil samples)")
+
 subplot(1, 2, 2); hist(errc, 50); title("Corrected Error");
+xlabel("Distance Between Predicted and Simulated Center (m)");
+ylabel("Count (N = 2.3 mil samples)")
+
+figure
+scatter(step.time, err, 10, 'Filled'); hold on
+scatter(step.time, errc, 10, 'Filled');
+title("Error vs. Time")
+xlabel("Time (s)")
+ylabel("Distance Between Centers (m)")
+legend(["Raw Error" "Corrected Error"])
+
--- trapping.m
+++ trapping.m
@@ -9,19 +9,63 @@ elseif oldsrc ~= src
 end
 oldsrc = src;
 
-% Constants
-c = 3e8;
-factor = 1.820618101874320e-30;
-m = 9.109e-31;
-v = 2.59627974e8;
-gamma = 1/sqrt(1 - (v^2)/(c^2));
-q = 1.602e-19;
-E = 200000;
-B = 1;
 
-for i=5:5
-    
-    % Get particle path data and plot x/y plane projection (for simple viz)
-    step_data = step(map.FIRST_STEP_INDEX(i) + 1 : map.LAST_STEP_INDEX(i), : );
-    plot(step_data.position_z, step_data.position_y); hold on;
+times_ahead = [0; step.position_z(1:end-1)];
+times = step.position_z;
+times_behind = [step.position_z(2:end); 0];
+turnaround = (abs(times_ahead) < abs(times)).*(abs(times_behind) < abs(times));
+
+m = arrayfun(@(x,y,z)  repmat(x + 1, z - y + 1, 1), map.TRACK_INDEX, map.FIRST_STEP_INDEX, map.LAST_STEP_INDEX, 'UniformOutput', false);
+m = vertcat(m{:});
+
+step.track_id = m;
+turn_data = step(turnaround == 1,:);
+
+turnlist = arrayfun(@(x) table2array(turn_data(turn_data.track_id == (x + 1),'time')), map.TRACK_INDEX, 'UniformOutput', false);
+zlist = arrayfun(@(x) table2array(turn_data(turn_data.track_id == (x + 1),'position_z')), map.TRACK_INDEX, 'UniformOutput', false);
+
+turntimes = arrayfun(@(x) mean(diff(x{1, 1}(2:end-1))), turnlist);
+turnz = arrayfun(@(x) mean(abs(diff(x{1, 1}(2:end-1)))), zlist);
+track.turn_time = turntimes;
+track.turn_z = turnz;
+
+disp(["Trapping Fraction " num2str(size(track(track.terminator_name == "term_max_steps", :), 1) / size(track, 1))]);
+
+figure;
+scatter(step.position_z, step.magnetic_field_z);
+title("Magnetic Field")
+xlabel("z position (m)")
+ylabel("B-Field z-component (T)")
+
+figure;
+scatter(track.initial_polar_angle_to_z, track.turn_time);
+title("Oscillation Time vs. Pitch Angle")
+xlabel("Pitch Angle (degrees)");
+ylabel("Elapsed Time Between Classical Turning Points (s)");
+
+figure;
+scatter(track.initial_polar_angle_to_z, track.turn_z);
+title("Oscillation Width vs. Pitch Angle");
+xlabel("Pitch Angle (degrees)");
+ylabel("Distance Between Classical Turning Points (m)");
+
+
+%% Trapping Fraction for Uniform Spatial Dist. 
+
+% Set simulation source
+src = "trap_uniform";
+
+% Load data
+if exist('oldsrc', 'var') == 0
+    [~, track, ~] = load_sim(src);
+elseif oldsrc ~= src
+    [~, track, ~] = load_sim(src);
 end
+oldsrc = src;
+
+
+disp(["Trapping Fraction " num2str(size(track(track.terminator_name == "term_max_steps", :), 1) / size(track, 1))]);
+disp(["Left Geometry" num2str(size(track(track.terminator_name == "term_max_z", :), 1) / size(track, 1))]);
+disp(["Larmor Exceeds Geometry" num2str(size(track(track.terminator_name == "term_max_r", :), 1) / size(track, 1))]);
+
+
